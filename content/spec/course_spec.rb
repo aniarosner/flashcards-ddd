6cc6407d6858a686b9e6d445f35d9b3a802f9379
Anna Rosner <ania.rosner@gmail.com>
2018-12-19 00:06:46 +0100

    Add Course aggregate tests

---
+++ content/spec/course_spec.rb
@@ -0,0 +1,28 @@
+module Content
+  RSpec.describe 'Course aggregate' do
+    specify 'create new course' do
+      course = Content::Course.new(course_english_grammar[:uuid])
+      course.register
+
+      expect(course).to have_applied(course_created)
+    end
+
+    private
+
+    def course_created
+      an_event(Content::CourseCreated).with_data(course_created_data).strict
+    end
+
+    def course_created_data
+      {
+        uuid: course_english_grammar[:uuid]
+      }
+    end
+
+    def course_english_grammar
+      {
+        uuid: 'e319e624-4449-4c90-9283-02300dcdd293'
+      }
+    end
+  end
+end
