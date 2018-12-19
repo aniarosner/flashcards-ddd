fa85d30467cb0fc3814718805d0d0755b3a18664
Anna Rosner <ania.rosner@gmail.com>
2018-12-19 23:08:34 +0100

    Create commands and domain_events directories

--- content/lib/content.rb
+++ content/lib/content.rb
@@ -1,2 +1,11 @@
 module Content
 end
+
+require_dependency 'content/commands/add_deck.rb'
+require_dependency 'content/commands/create_course.rb'
+require_dependency 'content/commands/remove_course.rb'
+require_dependency 'content/commands/set_course_title.rb'
+
+require_dependency 'content/domain_events/course_created.rb'
+require_dependency 'content/domain_events/course_removed.rb'
+require_dependency 'content/domain_events/course_title_set.rb'
