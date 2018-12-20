6bbe989692f324aa7a02ace074a5ffdd2a3cb933
Anna Rosner <ania.rosner@gmail.com>
2018-12-20 23:35:51 +0100

    Change file arrangement in content bc

--- content/lib/content.rb
+++ content/lib/content.rb
@@ -1,19 +1,24 @@
 module Content
 end
-require_dependency 'content/commands/add_deck_to_course.rb'
-require_dependency 'content/commands/create_course.rb'
-require_dependency 'content/commands/remove_course.rb'
-require_dependency 'content/commands/set_course_title.rb'
+require_dependency 'content/course_aggregate/commands/set_course_title.rb'
+require_dependency 'content/course_aggregate/commands/create_course.rb'
+require_dependency 'content/course_aggregate/commands/remove_course.rb'
+require_dependency 'content/course_aggregate/domain_events/course_created.rb'
+require_dependency 'content/course_aggregate/domain_events/course_removed.rb'
+require_dependency 'content/course_aggregate/domain_events/course_title_set.rb'
+require_dependency 'content/course_aggregate/course_command_handler.rb'
+require_dependency 'content/course_aggregate/course_state.rb'
+require_dependency 'content/course_aggregate/course.rb'
 
-require_dependency 'content/domain_events/course_created.rb'
-require_dependency 'content/domain_events/course_removed.rb'
-require_dependency 'content/domain_events/course_title_set.rb'
-require_dependency 'content/domain_events/deck_added_to_course.rb'
+require_dependency 'content/deck_aggregate/commands/add_deck_to_course.rb'
+require_dependency 'content/deck_aggregate/domain_events/deck_added_to_course.rb'
+require_dependency 'content/deck_aggregate/card.rb'
+require_dependency 'content/deck_aggregate/deck_command_handler.rb'
+require_dependency 'content/deck_aggregate/deck_state.rb'
+require_dependency 'content/deck_aggregate/deck.rb'
 
 require_dependency 'content/domain_services/course_presence_validator.rb'
 
 require_dependency 'content/read_models/course_list/course.rb'
 require_dependency 'content/read_models/course_list/event_handler.rb'
 require_dependency 'content/read_models/course_list_read_model.rb'
-
-require_dependency 'content/card.rb'
