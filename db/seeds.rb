01bd255fab434fdcb21e354079d8eec6e49058f3
Anna Rosner <ania.rosner@gmail.com>
2018-12-21 10:51:58 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+command_bus = Rails.configuration.command_bus
+
+english_grammar = { course_uuid: 'e319e624-4449-4c90-9283-02300dcdd293', title: 'English Grammar' }
+phrasal_verbs = { deck_uuid: '856a739c-e18c-4831-8958-695feccd2d73', title: 'Phrasal Verbs' }
+look_forward_to = { front: 'Look forward to', back: 'To be pleased about sth that is going to happen' }
+look_into = { front: 'Look into', back: 'To investigate, to research' }
+
+command_bus.call(
+  Content::CreateCourse.new(course_uuid: english_grammar[:course_uuid])
+)
+command_bus.call(
+  Content::SetCourseTitle.new(course_uuid: english_grammar[:course_uuid], title: english_grammar[:title])
+)
+command_bus.call(
+  Content::CreateDeckInCourse.new(deck_uuid: phrasal_verbs[:deck_uuid], course_uuid: english_grammar[:course_uuid])
+)
+command_bus.call(
+  Content::AddCardToDeck.new(deck_uuid: phrasal_verbs[:deck_uuid], front: look_forward_to[:front], back: look_forward_to[:back])
+)
+command_bus.call(
+  Content::AddCardToDeck.new(deck_uuid: phrasal_verbs[:deck_uuid], front: look_into[:front], back: look_into[:back])
+)
