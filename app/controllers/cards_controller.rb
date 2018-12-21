73bf48daf2e2ff7eea49aad57174aa8c8cab62b4
Anna Rosner <ania.rosner@gmail.com>
2018-12-21 01:23:24 +0100

    Add AddCardToDeck command to CardsController

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,6 +7,9 @@ class CardsController < ApplicationController
 
   def create
     respond_to do |format|
+      command_bus.call(
+        Content::AddCardToDeck.new(deck_uuid: params[:deck_uuid], front: params[:front], back: params[:back])
+      )
       format.json { head :no_content }
     end
   end
