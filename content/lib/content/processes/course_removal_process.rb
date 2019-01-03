fa9520b4fe232a7afde1f46f8bd7c3ccb9fdbdce
Anna Rosner <ania.rosner@gmail.com>
2019-01-03 15:10:51 +0100

    Add State class to CourseRemovalProcess

--- content/lib/content/processes/course_removal_process.rb
+++ content/lib/content/processes/course_removal_process.rb
@@ -10,7 +10,7 @@ module Content
       when Content::CourseRemoved
         remove_decks_from_course(event.data[:course_uuid])
       when Content::DeckCreatedInCourse, Content::DeckRemoved
-        store_event(event)
+        store_deck_information(event)
       end
     end
 
@@ -18,41 +18,59 @@ module Content
 
     def remove_decks_from_course(course_uuid)
       deck_uuids = load_deck_uuids(course_uuid)
+
       deck_uuids.each do |deck_uuid|
         @command_bus.call(Content::RemoveDeck.new(course_uuid: course_uuid, deck_uuid: deck_uuid))
       end
     end
 
     def load_deck_uuids(course_uuid)
-      events = @event_store.read.stream(stream_name(course_uuid))
-      deck_uuids = []
+      state = State.new(event_store: @event_store).load(course_uuid)
+      state.deck_uuids
+    end
 
-      events.each do |event|
+    def store_deck_information(event)
+      State.new(event_store: @event_store).store(event)
+    end
+
+    class State
+      def initialize(event_store:)
+        @event_store = event_store
+        @deck_uuids = []
+      end
+
+      attr_reader :deck_uuids
+
+      def apply(event)
         case event
         when Content::DeckCreatedInCourse
-          deck_uuids.push(event.data[:deck_uuid])
+          @deck_uuids.push(event.data[:deck_uuid])
         when Content::DeckRemoved
-          deck_uuids.delete(event.data[:deck_uuid])
+          @deck_uuids.delete(event.data[:deck_uuid])
         end
       end
 
-      deck_uuids
-    end
+      def load(course_uuid)
+        events = @event_store.read.stream(stream_name(course_uuid))
+        events.each { |event| apply(event) }
+        self
+      end
 
-    def store_event(event)
-      stream_name = stream_name(event.data[:course_uuid])
-      past_events_size = @event_store.read.stream(stream_name).to_a.size
-      last_stored_event = past_events_size - 1
+      def store(event)
+        stream_name = stream_name(event.data[:course_uuid])
+        past_events_size = @event_store.read.stream(stream_name).to_a.size
+        last_stored_event = past_events_size - 1
 
-      @event_store.link(
-        event.event_id, stream_name: stream_name, expected_version: last_stored_event
-      )
-    rescue RubyEventStore::WrongExpectedVersion
-      retry
-    end
+        @event_store.link(
+          event.event_id, stream_name: stream_name, expected_version: last_stored_event
+        )
+      rescue RubyEventStore::WrongExpectedVersion
+        retry
+      end
 
-    def stream_name(course_uuid)
-      "CourseRemoval$#{course_uuid}"
+      def stream_name(course_uuid)
+        "CourseRemoval$#{course_uuid}"
+      end
     end
   end
 end
